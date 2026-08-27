280c1f164b6144ea7f6ce983e03ad72f10eb0e91
Ellen Damasceno <ellen.damasceno@taya.com.br>
2026-08-27 15:12:09 -0300

    Corrige largura do modal de cancelar proposta no desktop

--- projetos/taya-app/src/components/UnicoAguardando.tsx
+++ projetos/taya-app/src/components/UnicoAguardando.tsx
@@ -164,7 +164,7 @@ export default function UnicoAguardando({
         </Drawer>
       ) : (
         <Dialog open={cancelarAberto} onOpenChange={setCancelarAberto}>
-          <DialogContent>
+          <DialogContent className="max-w-md">
             <DialogHeader>
               <DialogTitle>Cancelar proposta</DialogTitle>
             </DialogHeader>
